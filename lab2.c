c467d834ea1ccd88fd6ba2c0392718ee3216298e
anf2143 <anf2143@micro23.ee.columbia.edu>
2024-02-25 23:11:10 -0500

    made threading changes

--- lab2.c
+++ lab2.c
@@ -218,9 +218,14 @@ int done = 0;
   int keyidx = 0;
   int changed = 0;
 
+  char recvBuf[BUFFER_SIZE];
+  int n;
+  int message_row = 9;
+  int message_col = 0;
 
-
-
+  size_t len = 0;
+  int roll_scroll = 0;
+  int edit = 0; 
 
 int main()
 {
@@ -297,7 +302,7 @@ int main()
 
   
   /* Look for and handle keypresses */
-  for (;;) {
+ /* for (;;) {
     libusb_interrupt_transfer(keyboard, endpoint_address,
 			      (unsigned char *) &packet, sizeof(packet),
 			      &transferred, 0);
@@ -397,45 +402,60 @@ int main()
      }
      changed = 0;
      
-      if (packet.keycode[keyidx] == 0x29) { /* ESC pressed? */
+      if (packet.keycode[keyidx] == 0x29) {
 	break;
       }
     }
   }
   
-  free(sendBuf);
+
+*/ 
+
+
   
   /* Terminate the network thread */
-  pthread_cancel(network_thread);
+//  pthread_cancel(network_thread);
   
   /* Wait for the network thread to finish */
-  pthread_join(network_thread, NULL);
+  pthread_join(read_thread, NULL);
+  pthread_join(write_thread,NULL);
+  pthread_join(edit_thread,NULL);
 
+  free(sendBuf);
+  
   return 0;
 }
 
+
+
 void *network_thread_fread(void *ignored)
 {
-  char recvBuf[BUFFER_SIZE];
-  int n;
-  int message_row = 9;
-  int message_col = 0;
+do{
+ // char recvBuf[BUFFER_SIZE];
+ // int n;
+  message_row = 9;
+  message_col = 0;
   /* Receive data */
+ 
+ pthread_mutex_lock(&mut1);
+ 
+
+ while(wait_send){ pthread_cond_wait(&cond_wr,&mut1);}
+
+ 
   while ( (n = read(sockfd, &recvBuf, BUFFER_SIZE - 1)) > 0 ) {
     recvBuf[n] = '\0';
     printf("%s", recvBuf);
-    size_t len = 0;
-        /*if (sendBuf[BUFFER_SIZE] == '\0') {
-          len = strlen(sendBuf);
-        }
-        else {
-          len = BUFFER_SIZE;
-        }
-        if (len == 0) {
-          len++;
-        }*/
+    len = 0;
+    
+    
+    wait_send = 1;
+    edit = 0;
+    pthread_cond_signal(&cond_wait);
+    
+  /*      
         len = strlen(recvBuf);
-        int row_scroll = ((len-1)/64) + 1;
+        row_scroll = ((len-1)/64) + 1;
         for (int i = 0; i < len; i++) {
 	    if(message_row == 21) {
                fb_scroll(row_scroll);
@@ -459,18 +479,193 @@ void *network_thread_fread(void *ignored)
           message_row++;
         }
 
-  }
+	*/
 
+  }//end of while (n=read())
+ pthread_mutex_unlock(&disp_msg_mutex);
+
+} while(!done);
   return NULL;
-}
+} // end of fread
 
 
 
 void *network_thread_fenter (void *ignored){
+	while(!done) {
+
+libusb_interrupt_transfer(keyboard, endpoint_address,
+			      (unsigned char *) &packet, sizeof(packet),
+			      &transferred, 0);
+
+  if (transferred == sizeof(packet)) {
+   
+	pthread_mutex_lock(&mut1);
+	while(wait_send) {pthread_cond_wait(&cond_wr, &mut1) };
+
+      sprintf(keystate, "%08x %02x %02x", packet.modifiers, packet.keycode[0],
+	      packet.keycode[1]);
+      printf("%s\n", keystate);
+
+      for (int i = 0; i < 6; i++) {
+	if (old_keys[i] != packet.keycode[i]) {
+           keyidx = i;
+           changed = 1;
+           old_keys[i] = packet.keycode[i];
+        }
+      }
+
+      if (changed && packet.keycode[keyidx] != 0 && packet.keycode[keyidx] != 42 && packet.keycode[keyidx] != 40 && 
+          packet.keycode[keyidx] != 79 && packet.keycode[keyidx] != 80 && packet.keycode[keyidx] != 41) { //regular key
+        if (packet.modifiers == 0x02 || packet.modifiers == 0x20){ //shift
+          insert_key(sendBuf, usb2s_ascii[packet.keycode[keyidx]], input_row, input_col, BUFFER_SIZE);
+        }
+        else{ //no shift
+          insert_key(sendBuf, usb2ns_ascii[packet.keycode[keyidx]], input_row, input_col, BUFFER_SIZE);
+        }
+        if (!(input_col == 63 && input_row == 23) && sendBuf[BUFFER_SIZE-1] == '\0') { //not able to insert at 128
+          input_col++;
+          if (input_col == 64){
+            input_col = 0;
+            input_row++;
+          }
+        }
+      }
+      else if (changed && packet.keycode[keyidx] == 0){ //let go or no input
+        int idx = (input_row - 22)*64+input_col;
+        if (sendBuf[idx] == '\0'){
+          fbputchar(12, input_row,input_col);
+        }
+        else {
+          fbputinvertchar(sendBuf[idx], input_row, input_col);
+        }
+      }
+      else if (changed && packet.keycode[keyidx] == 42 && !(input_col == 0 && input_row == 22)){ //backspace
+        backspace(sendBuf, input_row, input_col, BUFFER_SIZE);
+        int idx = (input_row - 22)*64+input_col;
+        if(!(idx == 127 && sendBuf[idx] == '\0')){ //if you backspace at 128, you should insert again at 128
+          input_col--;
+          if (input_col < 0) {
+            input_col = 63;
+            input_row--;
+          }
+        }
+     }
+     else if (changed && packet.keycode[keyidx] == 79 && !(input_col == 63 && input_row == 23 )) { //right arrow
+        int left_input_row = input_row;
+        int left_input_col = input_col+ 1;
+        if (left_input_col == 64){
+        left_input_col = 0;
+          left_input_row++;
+        }
+        if (sendBuf[(left_input_row-22)*64+left_input_col] != '\0') {
+          fbputchar(sendBuf[(input_row-22)*64+input_col], input_row, input_col);
+          input_row = left_input_row;
+          input_col = left_input_col;
+          int idx = (input_row - 22)*64+input_col;
+          fbputinvertchar(sendBuf[idx], input_row, input_col);
+        }
+        else if (sendBuf[(left_input_row-22)*64+left_input_col] == '\0' && sendBuf[(input_row-22)*64+input_col] != '\0') {
+	      fbputchar(sendBuf[(input_row-22)*64+input_col], input_row, input_col);
+          input_row = left_input_row;
+          input_col = left_input_col;
+          fbputchar(12, input_row, input_col);
+        }
+     }
+     else if (changed && packet.keycode[keyidx] == 80  && !(input_col == 0 && input_row == 22)) { //left arrow
+        if (sendBuf[(input_row-22)*64+input_col] == '\0') {
+          fbputchar(' ', input_row, input_col);
+        }
+        else {
+          fbputchar(sendBuf[(input_row-22)*64+input_col], input_row, input_col);
+        }
+        input_col--;
+        if (input_col < 0) {
+          input_col = 63;
+          input_row--;
+        }
+        int idx = (input_row - 22)*64+input_col;
+        fbputinvertchar(sendBuf[idx], input_row, input_col);
+     }
+     else if (changed && packet.keycode[keyidx] == 40) { //enter
+
+     pthread_cond_signal(&cond_wait);
+     edit = 1;
+     wait_send = 1;
+/*
+	 clear_framebuff(22, 0);
+        input_row = 22;
+        input_col =0;
+        send(sockfd, sendBuf, strlen(sendBuf), 0);  
+        memset(sendBuf, '\0', BUFFER_SIZE); */
+
+     }
+     changed = 0;
+     
+      if (packet.keycode[keyidx] == 0x29) { /* ESC pressed? */
+	done = 1;
+      }
+    }
 
 
+	pthread_mutex_unlock(&mut1);
 
 
-
+	} //end of while (!done)
+	return NULL;
 }// end of network_thread_fenter
+
+
+
+
+void *network_thread_fwrite(void * ignored){
+ do{
+	while(!wait_send) {pthread_cond_wait(&cond_wait, &mut1); }
+	if (edit == 1){
+
+	 clear_framebuff(22, 0);
+        input_row = 22;
+        input_col =0;
+        send(sockfd, sendBuf, strlen(sendBuf), 0);  
+        memset(sendBuf, '\0', BUFFER_SIZE);
+
+
+	} //edit =1 
+	else {
+
+ 	len = strlen(recvBuf);
+        row_scroll = ((len-1)/64) + 1;
+        for (int i = 0; i < len; i++) {
+	    if(message_row == 21) {
+               fb_scroll(row_scroll);
+               message_row -= row_scroll;	
+            }
+            if (recvBuf[i] != ' ') {
+              printf("%c%d\n",recvBuf[i], i);
+           }
+           if (recvBuf[i] == '\0') {
+	      recvBuf[i] = ' ';
+           }
+           fbputchar(recvBuf[i], message_row, message_col);
+	    message_col++;
+	    if (message_col == 64){
+		message_col = 0;
+		message_row++;
+	    }     
+        }
+        if (message_col != 0) {
+          message_col = 0;
+          message_row++;
+        }
+
+	}//end else for edit == 0
+
+	pthread_cond_signal(&cond_wr);
+	wait_send = 0;
+
+	} while(!done);
+ 	return NULL; //thread termination
+
+} // end of write thread
+
+
 
